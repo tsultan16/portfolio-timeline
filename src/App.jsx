e7a61bb23c8e9011ee08ab9baa83c810f90f722e
tsultan16 <tanzids@student.unimelb.edu.au>
2025-03-05 15:54:36 +1100

    completed timeline for larger screen, smaller screen view still buggy

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import Timeline from './Timeline'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        Hello World!
-      </div>
+    <div className="flex flex-col justify-center items-center bg-gray-900 text-white text-base pb-8 sm:text-lg">
+        <Timeline defaultColor="bg-green-800" />
+    </div>
     </>
   )
 }
